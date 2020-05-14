d5613bf0c12c11aa904644fd741a97749566a11b
Dilawar Mahmood <dilawarm@stud.ntnu.no>
2020-05-14 09:49:10 +0200

    any_of

--- stl-algorithms/all_of.cpp
+++ stl-algorithms/all_of.cpp
@@ -13,8 +13,10 @@ bool all_of(InputIterator first, InputIterator last, UnaryPredicate pred) {
 
 int main() {
     vector<int> vec = {3,5,7,11,13,17,19,23};
+
     if (all_of(vec.begin(), vec.end(), [](int i) {
         return i % 2;
     })) cout << "All the elements are odd numbers." << endl;
-    else cout << "All the elements are not odd numbers." << endl;
+
+    return 0;
 }
---
+++ stl-algorithms/any_of.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+template<class InputIterator, class UnaryPredicate>
+bool any_of(InputIterator first, InputIterator last, UnaryPredicate pred) {
+    while (first != last) {
+        if (pred(*first)) return true;
+        ++first;
+    }
+    return false;
+}
+
+int main() {
+    vector<int> vec = {0,1,-1,3,-3,5,-5};
+    
+    if (any_of(vec.begin(), vec.end(), [](int i) {
+        return i < 0;
+    })) cout << "There are negative elements in the range" << endl;
+
+    return 0;
+}
