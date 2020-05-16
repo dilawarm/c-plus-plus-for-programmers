b6145994cd8b98262a2320fd706198c861fddc38
Dilawar Mahmood <dilawarm@stud.ntnu.no>
2020-05-16 09:32:40 +0200

    for_each

---
+++ stl-algorithms/for_each.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+template <class InputIterator, class Function>
+Function for_each(InputIterator first, InputIterator last, Function fn) {
+    while (first != last) {
+        fn(*first);
+        ++first;
+    }
+    return fn;
+}
+
+int main() {
+    vector<int> vec = {10, 20, 30};
+
+    cout << "vec contains:";
+    for_each(vec.begin(), vec.end(), [](int i) {
+        cout << " " << i;
+    });
+    cout << endl;
+
+    return 0;
+}
